88095a7117ff3bb84443509d9cef9bc3f36e6abe
nssahu786 <nssahu786@gmail.com>
2023-01-12 08:36:36 +0530

    initti

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(session({
   cookie: { secure: false }
 }));
 app.use(flash())
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 const web = require('./routes/web.js')   
 
