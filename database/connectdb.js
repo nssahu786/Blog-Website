86e709fbf8a7019fb57d64ab92864d74db517885
nssahu786 <nssahu786@gmail.com>
2023-01-12 08:58:04 +0530

    initti

--- database/connectdb.js
+++ database/connectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const con = "mongodb://nishant:123@ac-etggnag-shard-00-00.jmmeymf.mongodb.net:27017,ac-etggnag-shard-00-01.jmmeymf.mongodb.net:27017,ac-etggnag-shard-00-02.jmmeymf.mongodb.net:27017/webnishant?ssl=true&replicaSet=atlas-1ha579-shard-0&authSource=admin&retryWrites=true&w=majority"
+const con = "mongodb+srv://nishant:123@cluster0.jmmeymf.mongodb.net/test"
 
 const connectDB=()=>{
     //return mongoose.connect('mongodb://localhost:27017/project_blog') 
